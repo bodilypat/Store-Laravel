c805039917d85287d0fc8b7eaa81ebc55ea6878a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-11 15:12:28 -0700

    Create ProductService.php

---
+++ Backend/app/services/ProductService.php
@@ -0,0 +1,44 @@
+<!-- app/services/ProductService.php -->
+
+<?php
+namespace App\Services;
+
+use App\Models\Product;
+class ProductService
+{
+    public function getAllProducts()
+    {
+        return Product::all();
+    }
+
+    public function getProductById($id)
+    {
+        return Product::find($id);
+    }
+
+    public function createProduct($data)
+    {
+        return Product::create($data);
+    }
+
+    public function updateProduct($id, $data)
+    {
+        $product = Product::find($id);
+        if ($product) {
+            $product->update($data);
+            return $product;
+        }
+        return null;
+    }
+
+    public function deleteProduct($id)
+    {
+        $product = Product::find($id);
+        if ($product) {
+            $product->delete();
+            return true;
+        }
+        return false;
+    }
+}
+
